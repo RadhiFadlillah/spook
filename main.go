7d20b667fedfa755426145969351e1059d066a31
Radhi Fadlillah <m.radhi.f@gmail.com>
2020-08-15 17:18:05 +0700

    Remove everything

--- main.go
+++ main.go
@@ -1,12 +1,4 @@
 package main
 
-import (
-	"github.com/go-spook/spook/cmd"
-	"github.com/sirupsen/logrus"
-)
-
 func main() {
-	if err := cmd.SpookCmd().Execute(); err != nil {
-		logrus.Fatalln(err)
-	}
 }
